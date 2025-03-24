daf9f7999935f5166a4d52d1feb62d117294d244
Siddhesh Abhijeet Dhonde <sd1386@rit.edu>
2025-03-23 20:58:20 -0400

    Addede Auth endpoints for User and Admins

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,48 @@
 const { Router } = require("express");
-const adminModel = require("../db")
+const {adminModel} = require("../db")
 const adminRouter = Router();
 // adminRouter.use(adminMiddleware);
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "alsiwjendi";
 
-adminRouter.post("/signup", function(req, res){
+
+adminRouter.post("/signup", async function(req, res){
+    const {email, password, firstName, lastName} = req.body; // TODO: Add zod valdiation
+    // TODO: Hash the password using Bcrypt 
+    // Add try catch block
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+
     res.json({
         message: "Signup Complete"
     })
 })
 
-adminRouter.post("/signin", function(req, res){
-    res.json({
-        message: "Signin Complete"
+adminRouter.post("/signin", async function(req, res){
+    const {email, password} = req.body;
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
     })
+
+    if (admin){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_ADMIN_PASSWORD);
+
+        res.json({
+            token: token,
+        })
+    }
+    else{
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
 })
 
 adminRouter.post("/createCourse", function(req, res){
--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,46 @@
 const { Router } = require("express");
-
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "alsiwjendi";
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res){
+userRouter.post("/signup", async function(req, res){
+    const {email, password, firstName, lastName} = req.body; // TODO: Add zod valdiation
+    // TODO: Hash the password using Bcrypt 
+    // Add try catch block
+    await userModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+
     res.json({
         message: "Signup Complete"
     })
 })
 
-userRouter.post("/signin", function(req, res){
-    res.json({
-        message: "Signin Complete"
+userRouter.post("/signin", async function(req, res){
+    const {email, password} = req.body;
+    const user = await userModel.findOne({
+        email: email,
+        password: password
     })
+
+    if (user){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD);
+
+        res.json({
+            token: token,
+        })
+    }
+    else{
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
 })
 
 userRouter.get("/purchases", function(req, res){
